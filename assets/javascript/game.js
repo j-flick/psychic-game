491783f76c132534e37fda2f7a4ea6e81f47f998
Justin Flick <jflick84@gmail.com>
2017-09-08 20:44:10 -0400

    added pseudo code and basic html shell

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Create array of possible letters.
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Create variables for wins, losses, guesses left.
+
+// The app picks a letter at random.
+
+// Ask the user to pick a letter.
+
+// Add the letter to guesses so far.
+
+// Compare the letters to see if they match.
+
+// If they match, display 'You Win!' alert, increment wins, and start the game over.
+
+// If they do not match, increment losses, decrement guesses left, and start the game over.
+
